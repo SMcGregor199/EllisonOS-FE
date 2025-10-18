3505ee6ff62bf89495dbf7146f28f1fff7f59d39
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-10-18 09:07:55 -0400

    add initial files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+ const x : string = "Studies";
+
+  return (
+    <>
+      <h1>Ellison {x}</h1>
+      <p>Ellison is a {x}</p>
+    </>
+  )
+}
+
+export default App
