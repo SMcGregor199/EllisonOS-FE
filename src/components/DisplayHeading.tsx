744c1690d3a462f9676e59cbae0935de5c111070
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-11-18 11:50:12 -0500

    feat: add mobile responsiveness to display heading

--- src/components/DisplayHeading.tsx
+++ src/components/DisplayHeading.tsx
@@ -1,19 +1,15 @@
-import styled from "@emotion/styled"
 import {Typography, Flex} from "antd";
-
-const H1 = styled(Typography.Title)`
-    &&{
-        font-size:3rem;
-        word-break: initial;
-    }
-`
-export default function DisplayHeading(){
+export default function DisplayHeading(props){
+    const {screenSize} = props;
+    const headingFontSize = screenSize.md ? "3rem" : "4rem";
+    const paragraphFontSize = screenSize.md ? "1.2rem" : "1.5rem";
+    const flexSize = screenSize.md ? "0 0 35%" : "0 0 100%";
     return(
-        <Flex vertical style={{flex: "0 0 25%", justifyContent:"center"}}>
-            <H1 level={1}>
+        <Flex vertical style={{flex:flexSize, justifyContent:"center"}}>
+            <Typography.Title level={1} style={{fontSize:headingFontSize, wordBreak:"initial"}}>
                 The Ellison AI Project
-            </H1>    
-            <Typography.Paragraph style={{maxWidth:"32ch", fontSize:"1.2rem"}}>
+            </Typography.Title>    
+            <Typography.Paragraph style={{maxWidth:"32ch", fontSize:paragraphFontSize}}>
                 The Ellison AI Project is an experiment in whether we can teach a machine to be
                 an interlocutor to a human on the life and work of the late American novelist, Ralph Ellison.
             </Typography.Paragraph>
