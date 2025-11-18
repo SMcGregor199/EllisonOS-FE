9381ed5d0d9cac62a1b9f4222da24de9e6f644bc
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-11-18 18:39:19 -0500

    …

--- src/StaticLayout.tsx
+++ src/StaticLayout.tsx
@@ -7,7 +7,7 @@ export default function StaticLayout(){
     return(
         <Layout style={{paddingBlock: "2rem", minHeight:"100vh"}}>
             <Header />
-            <Content style={{paddingInline: 50, flexGrow: 1}} id="main">
+            <Content style={{paddingInline: 50, paddingBlock: 50, flexGrow: 1}} id="main">
                 <Outlet />
             </Content>
             <Footer />
