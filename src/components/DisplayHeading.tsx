78fc7989e4e8eb338eaffa25c8b507441cf40aca
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-11-18 16:02:04 -0500

    …

--- src/components/DisplayHeading.tsx
+++ src/components/DisplayHeading.tsx
@@ -9,6 +9,7 @@ export default function DisplayHeading(){
     const paragraphFontSize = isMd ? "1.2rem" : "1.5rem";
     const flexSize = isMd ? "0 0 35%" : "0 0 100%";
     return(
+        
         <Flex vertical style={{flex:flexSize, justifyContent:"center"}}>
             <Typography.Title level={1} style={{fontSize:headingFontSize, wordBreak:"initial"}}>
                 The Ellison AI Project
