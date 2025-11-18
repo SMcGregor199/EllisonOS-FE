e314d9578b51aa9303b2ccbb12fcd9a6f4669368
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-11-18 11:48:36 -0500

    feat: add social btns and copywright to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,34 @@
-import { Layout, Anchor} from "antd";
+import { Layout, Button, Flex, Typography} from "antd";
+import {GithubOutlined, LinkedinOutlined} from "@ant-design/icons";
 export default function Footer(){
     return(
         <Layout.Footer>
-            <Anchor direction="horizontal" style={{fontSize:"1rem"}} items={[
-                {key:"about", href:"#about", title:"About"},
-                {key:"case-study", href:"#case-study", title:"Case Study"},
-                {key:"contact", href:"#contact", title:"Contact"},
-                {key:"blog-posts", href:"#blog-posts", title:"Blog Posts"},
-            ]}>
-   
-            </Anchor>
+            <Flex style={{justifyContent:"center", alignItems:"center"}} gap={"1rem"}>
+                <Button
+                type="default"
+                icon={<LinkedinOutlined />}
+                href="https://linkedin.com/in/shayne-mcgregor"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Open my LinkedIn profile in a new tab"
+                style={{backgroundColor:"transparent"}}
+            >
+            </Button>
+            <Button
+                type="default"
+                icon={<GithubOutlined />}
+                href="https://github.com/SMcGregor199"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Open my GitHub profile in a new tab"
+                style={{backgroundColor:"transparent"}}
+                >
+                </Button>
+                <Typography.Text style={{ display: "block" }}>
+                    © {new Date().getFullYear()} Shayne McGregor
+                </Typography.Text>
+            </Flex>
+
         </Layout.Footer>
     )
 }
